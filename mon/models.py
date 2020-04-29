03c3f0c1b60a0e16efdbb21d26f3dd6808bb849a
Vlad Utkin <vlad@teamfnd.ru>
2020-04-29 16:24:30 +0300

    Fix admin model view

--- mon/models.py
+++ mon/models.py
@@ -14,7 +14,7 @@ class MachineRecord(models.Model):
     work_time = models.PositiveIntegerField()
 
     def __str__(self):
-        return self.date_captured
+        return self.date_captured.isoformat()
 
     class Meta:
         ordering = ('date_captured',)
